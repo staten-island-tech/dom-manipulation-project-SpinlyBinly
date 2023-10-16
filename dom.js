2667b40043455837c313720b0ffb6c371d24beb4
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-10-16 13:12:16 -0400

    commit 3

--- dom.js
+++ dom.js
@@ -1,16 +1,16 @@
-const DOMSelectors = {};
+/* const DOMSelectors = {};
 const form = document.querySelector("#form");
 const firstName = document.querySelector;
-console.log(firstName);
+console.log(firstName); */
 Domelectors = {
   form: document.querySelector("#form"),
-  firstName: document.querySelector("#.first-name"),
+  firstName: document.querySelector("#first-name"),
   h2s: document.querySelectorAll("h2"),
 };
 
 DomSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(DOMSelectors.firstName.value);
+  /*   console.log(DOMSelectors.firstName.value);*/
   DOMSelectors.h2s.forEach(
     (header) => (header.textContent = DomSelectors.firstName.value)
   );
