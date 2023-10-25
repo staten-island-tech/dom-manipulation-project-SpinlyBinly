b5c2c1a631d4dd379f9129aa42975eb5e967ebf5
SpinlyBinly <jane.laduca@gmail.com>
2023-10-25 18:07:16 -0400

    commit 7

--- dom.js
+++ dom.js
@@ -1,24 +1,23 @@
-/* const form = document.querySelector("#form");
+const form = document.querySelector("#form");
 const firstName = document.querySelector;
-console.log(firstName); */
+console.log(firstName); 
 
 //Step 1
 const DOMSelectors = [
   {
     form: document.querySelector("#form"),
-    firstName: document.querySelector("#form-first"),
+    bookTitle: document.querySelector(`${book - Title}`),
     h2s: document.querySelectorAll("h2"),
   },
 ];
 
-//Step 2 
+//Step 2
 function makeCard() {
   return `
-  <div class="card" id="XI">
-    <h2 class="${}"></h2>
-    <img src=""
-        alt="r" class="">
-    <h3>youre either on a health kick or hate yourself</h3>
+  <div class="card" id="cardID">
+    <h2 class= book-Title></h2>
+    <img src="https://i5.walmartimages.com/asr/61fab6ee-88fd-43de-9245-e7d9edd2e5dc.29470e3b2597ac84b8ff8b395b342791.jpeg "
+        alt="Sounder" class="SounderBook">
 </div>
 
   `;
@@ -28,8 +27,8 @@ body.insertAdjacentHTML(position, text);
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(DOMSelectors.firstName.value);
+  console.log(DOMSelectors.bookTitle.value);
 });
 /*
-//Step 4 DOMSelectors.firstName.value = "";
+//Step 4 DOMSelectors.bookTitle.value = "";
  */
