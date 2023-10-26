8cc750fd13b8316f71252a4389c58f17480f6467
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-10-26 13:13:15 -0400

    commit 7

--- dom.js
+++ dom.js
@@ -1,35 +1,31 @@
-/* const form = document.querySelector("#form");
-const firstName = document.querySelector;
-console.log(firstName); */
+//Step 1 (Create an object called "DOMSelectors" to hold DOM Selectors)
+const DOMSelectors = {
+  form: document.querySelector("#form"),
+  firstName: document.querySelector("#form-first"),
+  h2s: document.querySelectorAll("h2"),
+};
 
-//Step 1
-const DOMSelectors = [
-  {
-    form: document.querySelector("#form"),
-    firstName: document.querySelector("#form-first"),
-    h2s: document.querySelectorAll("h2"),
-  },
-];
-
-//Step 2 
+//Step 2 (Create a function that creates an object and calls the following functions)
 function makeCard() {
   return `
   <div class="card" id="XI">
-    <h2 class="${}"></h2>
+    <h2 class=""></h2>
     <img src=""
         alt="r" class="">
-    <h3>youre either on a health kick or hate yourself</h3>
+    <h3> </h3>
 </div>
 
   `;
 }
-//Step 3
-body.insertAdjacentHTML(position, text);
+//Step 3 (Create a function that injects the newly created object into the DOM)
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(DOMSelectors.firstName.value);
 });
 /*
-//Step 4 DOMSelectors.firstName.value = "";
+//Step 4 (Create a function that clears he input fields after injecting the object)
+ DOMSelectors.bookTitle.value = "";
  */
+
+//Step 5 (Create a function to remove an object after they have been created)
