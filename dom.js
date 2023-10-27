fa659c3d9bf8ec9a00af4d5ee88186a07b8cb076
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-10-27 13:14:35 -0400

    commit 10

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
 //Step 1 (Create an object called "DOMSelectors" to hold DOM Selectors)
 const DOMSelectors = {
   form: document.querySelector("#form"),
-  firstName: document.querySelector("#form-first"),
+  bookTitle: document.querySelector("#bookTitle"),
   h2s: document.querySelectorAll("h2"),
 };
 
@@ -10,21 +10,23 @@ function makeCard() {
   const form = document.querySelector("#form");
   const firstName = document.querySelector;
   console.log(firstName);
-
-  const DOMSelectors = {
-    form: document.querySelector("#form"),
-    bookTitle: document.querySelector(bookTitle),
-    h2s: document.querySelectorAll("h2"),
   };
-}
-//Step 3 Create a function that injects the newly created object into the DOM
-
+  
+  //Step 3 Create a function that injects the newly created object into the DOM
 DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log(DOMSelectors.bookTitle.value);
+  event.preventDefault(),
+  console.log(DOMSelectors.bookTitle.value),
 });
 
 //Step 4 (Create a function that clears he input fields after injecting the object)
 DOMSelectors.bookTitle.value = "";
 
 //Step 5 (Create a function to remove an object after they have been created)
+function remove(){
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach((btn))=>
+   btn.addEventListener("click", function (event) {
+    console.log(event.target.parentElement.parentElement);
+   })
+  }
+  remove()
