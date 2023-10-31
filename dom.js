8e026af97802f22262df6b8e4dea71a86db76300
SpinlyBinly <jane.laduca@gmail.com>
2023-10-30 22:19:14 -0400

    commit 12

--- dom.js
+++ dom.js
@@ -1,22 +1,20 @@
  //Step 1 (Create an object called "DOMSelectors" to hold DOM Selectors)
 const DOMSelectors = {
-  form: document.querySelector("#form"),
+  formID: document.querySelector("#formID"),
   bookTitle: document.querySelector("#bookTitle"),
   h2s: document.querySelectorAll("h2"),
+  container: document.querySelector(".container"),
 };
 
 //Step 2 (Create a function that creates an object and calls the following functions)
 function makeCard() {
-  const form = document.querySelector("#form");
-  const firstName = document.querySelector;
-  console.log(firstName);
+   DOMSelectors.container.insertAdjacentHTML("beforebegin", `<h1>Book title: ${bookTitle.value} </h1>`); 
   };
   
   //Step 3 Create a function that injects the newly created object into the DOM
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault(),
-  console.log(DOMSelectors.bookTitle.value),
-});
+DOMSelectors.formID.addEventListener("submit", function(){
+makeCard()
+  });
 
 //Step 4 (Create a function that clears the input fields after injecting the object)
 DOMSelectors.bookTitle.value = "";
@@ -24,8 +22,10 @@ DOMSelectors.bookTitle.value = "";
 //Step 5 (Create a function to remove an object after they have been created)
 function remove(){
   let buttons = document.querySelectorAll("button");
-  buttons.forEach((btn)) => btn.addEventListener("click", function (event) {
+  buttons.forEach((btn) => {btn.addEventListener("click", function (event)
+   {
     console.log(event.target.parentElement.parentElement);
-   })
-  }
+   })})};
   remove() 
+
+  
