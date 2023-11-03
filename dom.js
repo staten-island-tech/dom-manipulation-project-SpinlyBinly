aeb3e255e4e86e16edba7ad405d836b67fd1d3fe
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-11-03 11:41:59 -0400

    commit 14

--- dom.js
+++ dom.js
@@ -14,8 +14,8 @@ function makeCard() {
     "beforebegin",
     `<div class=card ><h2>Book Title: ${bookTitle.value} </h2>
     <h2> Book Author: ${bookAuthor.value} </h2>
-    <h2> Book Cover: ${bookCover.value}</h2>
-    <img src="https://prodimage.images-bn.com/pimages/9781338741063_p0_v2_s1200x630.jpg"> </div>`
+     Book Cover: ${bookCover.value}
+    <img src=""> </div>`
   );
 }
 
@@ -26,7 +26,7 @@ DOMSelectors.formID.addEventListener("submit", function (event) {
 });
 
 //Step 4 (Create a function that clears the input fields after injecting the object)
-
+DOMSelectors.bookTitle.value = "";
 //Step 5 (Create a function to remove an object after they have been created)
 function remove() {
   let buttons = document.querySelectorAll("button");
