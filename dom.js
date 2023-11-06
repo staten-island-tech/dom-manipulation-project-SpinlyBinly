c4c4300914134701cb62c7cbbc1da1d8eb734e92
SpinlyBinly <jane.laduca@gmail.com>
2023-11-05 22:38:27 -0500

    commit 15

--- dom.js
+++ dom.js
@@ -1,23 +1,24 @@
-//Step 1 (Create an object called "DOMSelectors" to hold DOM Selectors)
+b//Step 1 (Create an object called "DOMSelectors" to hold DOM Selectors)
 const DOMSelectors = {
   formID: document.querySelector("#formID"),
   bookTitle: document.querySelector("#bookTitle"),
-  h2s: document.querySelectorAll("h2"),
-  container: document.querySelector(".container"),
   bookAuthor: document.querySelector("#bookAuthor"),
   bookCover: document.querySelector("#bookCover"),
+  h2s: document.querySelectorAll("h2"),
+  container: document.querySelector(".container"),
+  button: document.querySelector(".btn")
 };
 
 //Step 2 (Create a function that creates an object and calls the following functions)
 function makeCard() {
   DOMSelectors.container.insertAdjacentHTML(
-    "beforebegin",
-    `<div class=card ><h2>Book Title: ${bookTitle.value} </h2>
-    <h2> Book Author: ${bookAuthor.value} </h2>
-     Book Cover: ${bookCover.value}
-    <img src=""> </div>`
-  );
-}
+    "beforeend", 
+    `<div class=card>
+    <h2>Book Title: ${bookTitle.value} </h2>
+    <h2> Book Author: ${bookAuthor.value} </h2>  
+    <img src="${bookCover.value}" alt="">
+    <button class=btn">Remove</button></div>`
+   )};
 
 //Step 3 Create a function that injects the newly created object into the DOM
 DOMSelectors.formID.addEventListener("submit", function (event) {
@@ -27,13 +28,16 @@ DOMSelectors.formID.addEventListener("submit", function (event) {
 
 //Step 4 (Create a function that clears the input fields after injecting the object)
 DOMSelectors.bookTitle.value = "";
+DOMSelectors.bookAuthor.value = "";
+DOMSelectors.bookCover.value = "";
 //Step 5 (Create a function to remove an object after they have been created)
-function remove() {
+function removeObject() {
   let buttons = document.querySelectorAll("button");
   buttons.forEach((btn) => {
     btn.addEventListener("click", function (event) {
-      console.log(event.target.parentElement.parentElement);
+      console.log(event.target.parentElement);
     });
   });
 }
-remove();
+removeObject();
+
